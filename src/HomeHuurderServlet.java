7d1c0a41370176a4b5b2798319ade16008aefe9a
Ruben Assink <rubenassink@gmail.com>
2016-09-10 20:55:50 +0200

    huurder verhuurder login + redirect naar home

---
+++ src/HomeHuurderServlet.java
@@ -0,0 +1,21 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * Created by rubenassink on 09-09-16.
+ */
+@WebServlet("/homehuurder")
+public class HomeHuurderServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/WEB-INF/homeHuurder.html").forward(request, response);
+    }
+}
--- src/HomeVerhuurderServlet.java
+++ src/HomeVerhuurderServlet.java
@@ -3,19 +3,17 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-/**
- * Created by rubenassink on 09-09-16.
- */
-@WebServlet("/home")
-public class HomeServlet extends HttpServlet {
+
+@WebServlet("/homeverhuurder")
+public class HomeVerhuurderServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        getServletContext().getRequestDispatcher("/WEB-INF/homeHuurder.html").forward(request, response);
+        getServletContext().getRequestDispatcher("/WEB-INF/homeVerhuurder.html").forward(request, response);
+
     }
 }
